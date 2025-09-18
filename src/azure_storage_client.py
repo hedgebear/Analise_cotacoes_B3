59b482162d8b40d5c6f86c85596b3944acea5581
Lucas Fernandes Mosqueira <lucas2002mkx@gmail.com>
2025-09-17 21:51:05 -0300

    ref: fix URL format in build_url_download function and improve container creation error handling

--- src/azure_storage_client.py
+++ src/azure_storage_client.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+CONTAINER_NAME = "pregao_raw"
 
 class StorageService():
     def __init__(self):
@@ -12,8 +13,17 @@ class StorageService():
             raise ValueError("Connection String para o Azure Storage não definida.")
 
         self.storage_service_client = BlobServiceClient.from_connection_string(self.connection_string)
+        self.container = self.storage_service_client.get_container_client(CONTAINER_NAME)
 
     def upload_blob_file(self, container_name: str, file_name: str, file_content: bytes):
+
+        try:
+            self.storage_service_client.create_container(container_name)
+            print(f"Container {container_name} criado com sucesso.")
+        except Exception as e:
+            print(f"Container {container_name} já existe ou erro ao criar: {e}")
+            pass
+
         blob_client = self.storage_service_client.get_blob_client(container=container_name, blob=file_name)
         blob_client.upload_blob(data=file_content, overwrite=True)
 
