6bbc861601cda0a084326f88c564f90eca9bad24
Gustavo Valentim <gustavovalentim09@gmail.com>
2025-10-01 18:14:48 -0300

    ref: alteração temporaria que salva localmente o arquivo

--- src/extract.py
+++ src/extract.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+from pathlib import Path
 from utils import convert_to_yymmdd
 import requests
 import os
@@ -21,25 +22,19 @@ def try_http_download(url:str):
         print(f"Falha ao acessar a {url}: {e}")
         pass
 
-def extract_files_from_nested_zip(zip_bytes):
-    files = []
-
+def extract_file_from_nested_zip(zip_bytes):
     with zipfile.ZipFile(io.BytesIO(zip_bytes)) as external_z:
         for external_file_name in external_z.namelist():
-            if external_file_name.lower().endswith('.zip'):            
+            if external_file_name.lower().endswith('.zip'):
                 internal_zip_bytes = external_z.read(external_file_name)
 
-                with zipfile.ZipFile(io.BytesIO(internal_zip_bytes)) as internal_z:            
+                with zipfile.ZipFile(io.BytesIO(internal_zip_bytes)) as internal_z:
                     for final_file_name in internal_z.namelist():
-                        if final_file_name.endswith('/'):
-                            continue
-                            
-                        file_content = internal_z.read(final_file_name)
-                        files.append((final_file_name, file_content))
-            
-            break
-                        
-    return files
+                        if not final_file_name.endswith('/'):
+                            file_content = internal_z.read(final_file_name)
+                            return final_file_name, file_content
+
+    return None, None
 
 def extract_files_from_zip(zip_bytes):
     files = []
@@ -54,8 +49,10 @@ def extract_files_from_zip(zip_bytes):
 
     return files
 
+PATH_TO_SAVE = "./dados_b3/PREGAO_RAW"
+
 def run():
-    storage_service = StorageService()
+    #storage_service = StorageService()
 
     dt = convert_to_yymmdd(datetime.now() - timedelta(days=1))
     url_to_download = build_url_download(dt)
@@ -66,12 +63,26 @@ def run():
 
     print(f"Download do arquivo de cotações realizado com sucesso")
 
+    file_name, file_content = extract_file_from_nested_zip(zip_bytes=zip_bytes)
+
+    file_path = f"{PATH_TO_SAVE}/{dt}/{file_name}"
+
+    directory = Path(file_path).parent
+
+    directory.mkdir(parents=True, exist_ok=True)
+
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+
+    """
     extracted_files = extract_files_from_zip(zip_bytes)
     for file_name, file_content in extracted_files:
         blob_path = f"dados_b3/{dt}/{file_name}"
         storage_service.upload_blob_file(container_name="pregao-raw", file_name=blob_path, file_content=file_content)
 
     print("Arquivos enviados para o Blob Storage com sucesso.")
+    """
+
 
 if __name__ == "__main__":
     run()
