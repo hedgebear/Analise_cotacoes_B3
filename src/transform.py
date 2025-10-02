b1f998f7c3022c551261d4c6689996050be98183
Gustavo Valentim <gustavovalentim09@gmail.com>
2025-10-02 11:38:01 -0300

    ref: extração dos dados do xml através das tags

--- src/transform.py
+++ src/transform.py
@@ -40,17 +40,17 @@ def transform():
         dados_extraidos = []
 
         for _, element in context:
-            ticker_el = element.find(f"{attributes_namespace}:SctyId/{attributes_namespace}:TckrSymb")
-            data_negociacao_el = element.find(f"{attributes_namespace}:TradDt/{attributes_namespace}:Dt")
+            ticker_el = element.find(f"{attributes_namespace}SctyId/{attributes_namespace}TckrSymb")
+            data_negociacao_el = element.find(f"{attributes_namespace}TradDt/{attributes_namespace}Dt")
             
-            preco_abertura_el = element.find(f"{attributes_namespace}:FinInstrmAttrbts/{attributes_namespace}:FrstPric")
-            preco_fechamento_el = element.find(f"{attributes_namespace}:FinInstrmAttrbts/{attributes_namespace}:LastPric")
+            preco_abertura_el = element.find(f"{attributes_namespace}FinInstrmAttrbts/{attributes_namespace}FrstPric")
+            preco_fechamento_el = element.find(f"{attributes_namespace}FinInstrmAttrbts/{attributes_namespace}LastPric")
 
-            preco_maximo_el = element.find(f"{attributes_namespace}:FinInstrmAttrbts/{attributes_namespace}:MaxPric")
-            preco_minimo_el = element.find(f"{attributes_namespace}:FinInstrmAttrbts/{attributes_namespace}:MinPric")
-            preco_medio_el = element.find(f"{attributes_namespace}:FinInstrmAttrbts/{attributes_namespace}:TradAvrgPric")
+            preco_maximo_el = element.find(f"{attributes_namespace}FinInstrmAttrbts/{attributes_namespace}MaxPric")
+            preco_minimo_el = element.find(f"{attributes_namespace}FinInstrmAttrbts/{attributes_namespace}MinPric")
+            preco_medio_el = element.find(f"{attributes_namespace}FinInstrmAttrbts/{attributes_namespace}TradAvrgPric")
 
-            quantidade_movimentada_el = element.find(f"{attributes_namespace}:FinInstrmAttrbts/{attributes_namespace}:RglrTxsQty")
+            quantidade_movimentada_el = element.find(f"{attributes_namespace}FinInstrmAttrbts/{attributes_namespace}RglrTxsQty")
 
             ticker = ticker_el.text if ticker_el is not None else None
             
@@ -59,11 +59,11 @@ def transform():
                     'ticker': ticker,
                     'data_negociacao': data_negociacao_el.text if data_negociacao_el is not None else None,
                     
-                    'preco_abertura': Decimal(preco_abertura_el.text) if preco_abertura_el is not None and preco_abertura_el.text else None,
-                    'preco_fechamento': Decimal(preco_fechamento_el.text) if preco_fechamento_el is not None and preco_fechamento_el.text else None,
-                    'preco_maximo': Decimal(preco_maximo_el.text) if preco_maximo_el is not None and preco_maximo_el.text else None,
-                    'preco_minimo': Decimal(preco_minimo_el.text) if preco_minimo_el is not None and preco_minimo_el.text else None,
-                    'preco_medio': Decimal(preco_medio_el.text) if preco_medio_el is not None and preco_medio_el.text else None,
+                    'preco_abertura': float(preco_abertura_el.text) if preco_abertura_el is not None and preco_abertura_el.text else None,
+                    'preco_fechamento': float(preco_fechamento_el.text) if preco_fechamento_el is not None and preco_fechamento_el.text else None,
+                    'preco_maximo': float(preco_maximo_el.text) if preco_maximo_el is not None and preco_maximo_el.text else None,
+                    'preco_minimo': float(preco_minimo_el.text) if preco_minimo_el is not None and preco_minimo_el.text else None,
+                    'preco_medio': float(preco_medio_el.text) if preco_medio_el is not None and preco_medio_el.text else None,
                     
                     'quantidade_negocios': int(quantidade_movimentada_el.text) if quantidade_movimentada_el is not None and quantidade_movimentada_el.text else None
                 }
