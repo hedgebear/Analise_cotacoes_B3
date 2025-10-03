ddc181e94931a1d0d12811ac81b87fd0bba58b56
Lucas Fernandes Mosqueira <lucas2002mkx@gmail.com>
2025-10-03 19:41:14 -0300

    feat: add SQL table creation script and commands for setup

---
+++ create_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS ativos_b3 (
+    ticker VARCHAR(20),
+    data_negociacao DATE,
+    preco_abertura NUMERIC,
+    preco_fechamento NUMERIC,
+    preco_maximo NUMERIC,
+    preco_minimo NUMERIC,
+    volume_financeiro NUMERIC
+);
