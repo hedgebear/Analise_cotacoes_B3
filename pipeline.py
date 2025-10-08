2984630aaf7b649d9f13099c0325af6f0278e588
Gustavo Valentim <gustavovalentim09@gmail.com>
2025-10-08 14:43:07 -0300

    organização da pipeline

--- pipeline.py
+++ pipeline.py
@@ -1,16 +1,18 @@
 from src.extract import run as extract_run
 from src.transform import transform
+from src.load import load_ativos
 import time
 
 def main():
-    print("Iniciando extração do arquivo da B3...")
-    extract_run(7)
+    print("Iniciando extração dos arquivos da B3.")
+    extract_run(qtd_dias_anteriores_a_baixar=7)
     print("Extração e upload para Azurite concluídos.")
-    print("Aguardando Azurite estar pronto...")
+    print("Aguardando Azurite estar pronto.")
     time.sleep(5)
-    print("Iniciando transformação e carga no banco de dados...")
-    transform(7)
-    print("Processo completo!")
+    print("Iniciando transformação e carga no banco de dados.")
+    dados_pregao = transform(qtd_dias_anteriores_a_baixar=7)
+    load_ativos(dados_pregao)
+
 
 if __name__ == "__main__":
     main()
