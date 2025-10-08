38c95b8a4e32b677a44c093a47c3f9c24d84f235
Gustavo Valentim <gustavovalentim09@gmail.com>
2025-10-08 17:03:18 -0300

    ref: limpeza da pipeline

--- pipeline.py
+++ pipeline.py
@@ -4,12 +4,8 @@ from src.load import load_ativos
 import time
 
 def main():
-    print("Iniciando extração dos arquivos da B3.")
     extract_run(qtd_dias_anteriores_a_baixar=7)
-    print("Extração e upload para Azurite concluídos.")
-    print("Aguardando Azurite estar pronto.")
     time.sleep(5)
-    print("Iniciando transformação e carga no banco de dados.")
     dados_pregao = transform(qtd_dias_anteriores_a_baixar=7)
     load_ativos(dados_pregao)
 
