98e264ae10f513537d2e17e03395b120df85b3a7
Gustavo Valentim <gustavovalentim09@gmail.com>
2025-10-07 21:19:47 -0300

    alteração no nome da main.py para pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -4,12 +4,12 @@ import time
 
 def main():
     print("Iniciando extração do arquivo da B3...")
-    extract_run()
+    extract_run(7)
     print("Extração e upload para Azurite concluídos.")
     print("Aguardando Azurite estar pronto...")
-    time.sleep(5)  # Pequeno delay para garantir que o Azurite esteja pronto
+    time.sleep(5)
     print("Iniciando transformação e carga no banco de dados...")
-    transform()
+    transform(7)
     print("Processo completo!")
 
 if __name__ == "__main__":
